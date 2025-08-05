8712ef5631d6be754d826bc547bf7409845b1edf
jiangtao.young <jiangtao.young@qq.com>
2025-08-05 21:49:57 +0800

    fix: demo error

--- apps/website/components/gallery/chartType/line.tsx
+++ apps/website/components/gallery/chartType/line.tsx
@@ -10,7 +10,7 @@ export const LineChart = memo(() => {
       { date: '2020', profit: 30, sales: 60 },
       { date: '2021', profit: 30, sales: 60 },
       { date: '2022', profit: 50, sales: 100 },
-      { date: '2023', profit: 40, sales: null },
+      { date: '2023', profit: 40, sales: 80 },
     ],
   }
   return <VSeedRender vseed={vseed} />

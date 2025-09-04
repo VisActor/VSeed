ee996557101d6e846c47e039983145722faee5a1
jiangtao.young <jiangtao.young@qq.com>
2025-09-04 21:27:03 +0800

    feat: update unit tests

---
+++ packages/vseed/tests/features/dataset/3m3d.test.ts
@@ -0,0 +1,15 @@
+import type { VSeed } from '@visactor/vseed'
+import { Builder, registerAll } from '@visactor/vseed'
+import vseed from './3m3d.json'
+
+test('3m3d', () => {
+  registerAll()
+  const builder = Builder.from(vseed as VSeed)
+  const advanced = builder.buildAdvanced()
+  if (!advanced) {
+    throw new Error('Failed to build advanced configuration')
+  }
+  const spec = builder.buildSpec(advanced)
+  expect(advanced).toMatchSnapshot()
+  expect(spec).toMatchSnapshot()
+});

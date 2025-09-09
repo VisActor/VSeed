2bff62f8fa01b4c62339522bbfe55cadf48e702c
jiangtao.young <jiangtao.young@qq.com>
2025-09-09 23:21:01 +0800

    fix: remove useless code

--- packages/vseed/src/pipeline/utils/dimensions/find.ts
+++ packages/vseed/src/pipeline/utils/dimensions/find.ts
@@ -1,33 +1,35 @@
 import type { Dimension, DimensionGroup, DimensionTree } from 'src/types'
 import { preorderTraverse } from '../tree'
 
-export const findDimensionById = (dimensions: DimensionTree = [], id: string): Dimension | undefined => {
-  if (!dimensions) return undefined
-  let result: Dimension | undefined
-  preorderTraverse<Dimension, DimensionGroup>(dimensions, (node) => {
-    if (!('children' in node)) {
-      if (node.id === id) {
-        result = node as Dimension
-        return true
-      }
-    }
-    return false
-  })
-  return result
-}
+// TODO: 未来可能有用的函数, 需要时再取消注释
+// export const findDimensionById = (dimensions: DimensionTree = [], id: string): Dimension | undefined => {
+//   if (!dimensions) return undefined
+//   let result: Dimension | undefined
+//   preorderTraverse<Dimension, DimensionGroup>(dimensions, (node) => {
+//     if (!('children' in node)) {
+//       if (node.id === id) {
+//         result = node as Dimension
+//         return true
+//       }
+//     }
+//     return false
+//   })
+//   return result
+// }
 
-export const findFirstDimension = (dimensions: DimensionTree = []): Dimension | undefined => {
-  if (!dimensions) return undefined
-  let result: Dimension | undefined
-  preorderTraverse<Dimension, DimensionGroup>(dimensions, (node) => {
-    if (!('children' in node)) {
-      result = node as Dimension
-      return true
-    }
-    return false
-  })
-  return result
-}
+// TODO: 未来可能有用的函数, 需要时再取消注释
+// export const findFirstDimension = (dimensions: DimensionTree = []): Dimension | undefined => {
+//   if (!dimensions) return undefined
+//   let result: Dimension | undefined
+//   preorderTraverse<Dimension, DimensionGroup>(dimensions, (node) => {
+//     if (!('children' in node)) {
+//       result = node as Dimension
+//       return true
+//     }
+//     return false
+//   })
+//   return result
+// }
 
 export const findAllDimensions = (dimensions: DimensionTree = []): Dimension[] => {
   if (!dimensions) return []

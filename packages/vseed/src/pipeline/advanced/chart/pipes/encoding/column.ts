3acf1a01ac3453af218e7908240922d53541f4a8
jiangtao.young <jiangtao.young@qq.com>
2025-09-12 18:03:24 +0800

    docs: update docs

--- packages/vseed/src/pipeline/advanced/chart/pipes/encoding/column.ts
+++ packages/vseed/src/pipeline/advanced/chart/pipes/encoding/column.ts
@@ -3,6 +3,25 @@ import { MeasureName } from 'src/dataReshape'
 import { findAllMeasures } from 'src/pipeline/utils'
 import type { AdvancedPipe, Dimension, Dimensions, Encoding, Measure, Measures } from 'src/types'
 
+/**
+ * @description 堆叠柱状图、并列柱状图、百分比柱状图
+ * 维度未包含任何encoding, 则使用默认映射规则:
+ * 1. x: 第一个维度映射至X轴
+ * 2. color: 非X轴所有维度与指标名称, 合并映射至颜色通道, 作为图例展示
+ * 3. detail: 其余所有维度映射至Detail通道
+ * 指标未包含任何encoding, 则使用默认映射规则:
+ * 1. y: 全部指标映射至Y轴
+ * 2. tooltip: 全部指标映射至Tooltip
+ *
+ * 维度映射规则:
+ * 1. 用户指定的xAxis维度映射至X轴, 支持多个维度; 若未指定, 则默认映射第一个维度
+ * 2. 用户指定的color维度映射至颜色通道, 支持多个维度; 若未指定, 则默认映射指标名称
+ * 3. 用户指定的detail维度映射至Detail通道, 支持多个维度; 若未指定, 则无detail
+ * 指标映射规则:
+ * 1. 指标未配置encoding, 则默认映射至Y轴;
+ * 2. 用户指定的yAxis指标映射至Y轴, 支持多个指标;
+ * 3. 所有指标均映射到Tooltip
+ */
 export const encodingForColumn: AdvancedPipe = (advancedVSeed) => {
   const { measures: vseedMeasures = [], dimensions = [] } = advancedVSeed
   const measures = findAllMeasures(vseedMeasures)
@@ -26,15 +45,12 @@ export const encodingForColumn: AdvancedPipe = (advancedVSeed) => {
   return { ...advancedVSeed, encoding }
 }
 
-const generateDefaultMeasureEncoding = (measures: Measures, encoding: Encoding) => {
-  encoding.tooltip = unique(measures.map((item) => item.id))
-  encoding.y = unique(measures.filter((item) => item.encoding === 'yAxis' || !item.encoding).map((item) => item.id))
-}
-
+/**
+ * --------------------维度--------------------
+ */
 const generateDefaultDimensionEncoding = (dimensions: Dimensions, encoding: Encoding) => {
   const onlyMeasureName = dimensions.length === 1 && dimensions.find((item) => item.id === MeasureName)
   const uniqueDimIds = unique(dimensions.map((d) => d.id))
-
   encoding.x = uniqueDimIds.slice(0, 1) // 第一个维度放置于X轴
   encoding.color = uniqueDimIds.slice(onlyMeasureName ? 0 : 1) // 第二个之后的维度用于颜色
   encoding.detail = uniqueDimIds.slice(onlyMeasureName ? 0 : 1) // 第二个之后的维度用于详情
@@ -43,7 +59,25 @@ const generateDefaultDimensionEncoding = (dimensions: Dimensions, encoding: Enco
   encoding.row = [] // 默认不进行行透视
   encoding.column = [] // 默认不进行列透视
 }
+const generateDimensionEncoding = (dimensions: Dimensions, encoding: Encoding) => {
+  encoding.x = unique(dimensions.filter((item) => item.encoding === 'xAxis').map((item) => item.id))
+  encoding.color = unique(dimensions.filter((item) => item.encoding === 'color').map((item) => item.id))
+  encoding.detail = unique(dimensions.filter((item) => item.encoding === 'detail').map((item) => item.id))
+  if (encoding.x.length === 0) {
+    encoding.x = [dimensions[0].id]
+  }
+  if (encoding.color.length === 0) {
+    encoding.color = [MeasureName]
+  }
+}
 
+/**
+ * --------------------指标--------------------
+ */
+const generateDefaultMeasureEncoding = (measures: Measures, encoding: Encoding) => {
+  encoding.tooltip = unique(measures.map((item) => item.id))
+  encoding.y = unique(measures.filter((item) => item.encoding === 'yAxis' || !item.encoding).map((item) => item.id))
+}
 const generateMeasureEncoding = (measures: Measures, encoding: Encoding) => {
   encoding.tooltip = measures.map((item) => item.id)
   encoding.y = unique(measures.filter((item) => item.encoding === 'yAxis' || !item.encoding).map((item) => item.id))
@@ -52,20 +86,4 @@ const generateMeasureEncoding = (measures: Measures, encoding: Encoding) => {
     encoding.color = color
   }
   return encoding
-}
-
-const generateDimensionEncoding = (dimensions: Dimensions, encoding: Encoding) => {
-  encoding.x = unique(dimensions.filter((item) => item.encoding === 'xAxis').map((item) => item.id))
-  encoding.color = unique(dimensions.filter((item) => item.encoding === 'color').map((item) => item.id))
-  encoding.detail = unique(dimensions.filter((item) => item.encoding === 'detail').map((item) => item.id))
-
-  if (encoding.x.length === 0) {
-    encoding.x = [dimensions[0].id]
-  }
-  if (encoding.color.length === 0) {
-    encoding.color = [MeasureName]
-  }
-  if (encoding.detail.length === 0) {
-    encoding.detail = dimensions.filter((item) => !encoding.x?.includes(item.id)).map((item) => item.id)
-  }
 }

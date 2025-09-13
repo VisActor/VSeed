545798df715535e994a97dcdc322f56962c4d613
jiangtao.young <jiangtao.young@qq.com>
2025-09-13 16:48:28 +0800

    feat: only secondary or only primary in dual axis

--- packages/vseed/src/pipeline/spec/chart/pipes/axes/yLinearPrimary.ts
+++ packages/vseed/src/pipeline/spec/chart/pipes/axes/yLinearPrimary.ts
@@ -2,7 +2,7 @@ import type { ISpec } from '@visactor/vchart'
 import { LINEAR_AXIS_INNER_OFFSET_TOP } from '../../../../utils/constant'
 import { autoFormatter } from '../../../../utils'
 import type { SpecPipe, YLinearAxis } from 'src/types'
-import { isEmpty } from 'remeda'
+import { isEmpty, isNullish } from 'remeda'
 
 export const yLinearPrimary: SpecPipe = (spec, context) => {
   const result = { ...spec } as ISpec
@@ -10,17 +10,18 @@ export const yLinearPrimary: SpecPipe = (spec, context) => {
   const { chartType } = vseed
   const { locale, datasetReshapeInfo } = advancedVSeed
   const { index, id: reshapeInfoId, foldInfoList } = datasetReshapeInfo[0]
-
   // TODO: default config missing
-
   const primaryYAxis = advancedVSeed.config?.[chartType as 'dualAxis']?.primaryYAxis as YLinearAxis | YLinearAxis[]
   const yAxisConfig = Array.isArray(primaryYAxis) ? primaryYAxis[index] || primaryYAxis[0] : primaryYAxis
   const alignTicks = advancedVSeed.config?.[chartType as 'dualAxis']?.alignTicks as boolean | boolean[]
   const alignTicksConfig = Array.isArray(alignTicks) ? alignTicks[index] || alignTicks[0] : alignTicks
 
-  if (foldInfoList?.[0] && isEmpty(foldInfoList[0].foldMap)) {
+  if (isNullish(foldInfoList?.[0])) {
     return result
   }
+
+  const isEmptySecondary = isEmpty(foldInfoList?.[0].foldMap)
+
   const id = `${reshapeInfoId}-primary-axis`
   const seriesIds = [`${reshapeInfoId}-primary-series`, `${reshapeInfoId}-secondary-series`]
   const seriesId = alignTicksConfig ? seriesIds : seriesIds[0]
@@ -61,7 +62,7 @@ export const yLinearPrimary: SpecPipe = (spec, context) => {
   } = yAxisConfig
 
   const linearAxis = {
-    visible,
+    visible: isEmptySecondary ? false : visible,
     id,
     seriesId,
 

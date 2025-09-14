6cbb4f9a42214ec885bfc29f5a1a289df2879231
jiangtao.young <jiangtao.young@qq.com>
2025-09-14 15:23:28 +0800

    fix: parentId for default group

--- packages/vseed/src/pipeline/utils/constant.ts
+++ packages/vseed/src/pipeline/utils/constant.ts
@@ -1,4 +1,4 @@
-// 标注层 z-index 
+// 标注层 z-index
 export const ANNOTATION_Z_INDEX = 1000
 // 线性轴 顶部空隙 预留5px
 export const LINEAR_AXIS_INNER_OFFSET_TOP = 7
@@ -6,3 +6,5 @@ export const LINEAR_AXIS_INNER_OFFSET_TOP = 7
 export const DUAL_AXIS_CHART_COLUMN_Z_INDEX = 1000
 // 双轴图 非柱图 ZIndex
 export const DUAL_AXIS_CHART_NON_COLUMN_Z_INDEX = 1001
+// 默认父级id
+export const DEFAULT_PARENT_ID = '__DefaultParentId__'

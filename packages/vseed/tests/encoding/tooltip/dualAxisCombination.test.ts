419d67daf607b8a55ee56e256206d41d035de345
jiangtao.young <jiangtao.young@qq.com>
2025-09-14 20:11:55 +0800

    feat: update tests

--- packages/vseed/tests/encoding/tooltip/dualAxisCombination.test.ts
+++ packages/vseed/tests/encoding/tooltip/dualAxisCombination.test.ts
@@ -10,52 +10,60 @@ VTable.register.chartModule('vchart', VChart)
 
 test('dualAxisCombination', () => {
   registerAll()
-  const builder = Builder.from(vseed as VSeed)
-  const advanced = builder.buildAdvanced()
-  if (!advanced) {
-    throw new Error('Failed to build advanced configuration')
-  }
-  const spec = builder.buildSpec(advanced)
 
-  const colorIdMap = builder.getColorIdMap()
-  const colorItems = builder.getColorItems()
-  const advancedPipeline = Builder.getAdvancedPipeline(builder.vseed.chartType)
-  const specPipeline = Builder.getSpecPipeline(builder.vseed.chartType)
-  const hasTheme = !!Builder.getTheme(builder.vseed.theme)
-  const themeMapKeys = Object.keys(Builder.getThemeMap())
+  try{
+    const builder = Builder.from(vseed as VSeed)
+    const advanced = builder.buildAdvanced()
+    if (!advanced) {
+      throw new Error('Failed to build advanced configuration')
+    }
+    let spec = builder.buildSpec(advanced)
+    spec = builder.build()
 
-  expect(advanced).toMatchSnapshot()
-  expect(spec).toMatchSnapshot()
-  expect({ colorIdMap, colorItems, advancedPipeline, specPipeline, hasTheme, themeMapKeys }).toMatchSnapshot()
+    const colorIdMap = builder.getColorIdMap()
+    const colorItems = builder.getColorItems()
+    const advancedPipeline = Builder.getAdvancedPipeline(builder.vseed.chartType)
+    const specPipeline = Builder.getSpecPipeline(builder.vseed.chartType)
+    const hasTheme = !!Builder.getTheme(builder.vseed.theme)
+    const themeMapKeys = Object.keys(Builder.getThemeMap())
 
-  const div = document.createElement('div') as unknown as HTMLDivElement
-  div.style.width = '960px'
-  div.style.height = '600px'
-  if (window) {
-    window.URL.createObjectURL = () => 'mocked-url'
-  }
+    expect(advanced).toMatchSnapshot()
+    expect(spec).toMatchSnapshot()
+    expect({ colorIdMap, colorItems, advancedPipeline, specPipeline, hasTheme, themeMapKeys }).toMatchSnapshot()
 
-  try {
-    // render
-    if (isPivotChart(builder.vseed)) {
-      const vtable = new VTable.PivotChart(div, spec as VTable.PivotChartConstructorOptions)
-      void vtable.updateOption(spec as VTable.PivotChartConstructorOptions)
-    } else if (isTable(builder.vseed)) {
-      const vtable = new VTable.ListTable(div, spec as VTable.ListTableConstructorOptions)
-      void vtable.updateOption(spec as VTable.ListTableConstructorOptions)
-    } else if (isPivotTable(builder.vseed)) {
-      const vtable = new VTable.PivotTable(div, spec as VTable.PivotTableConstructorOptions)
-      void vtable.updateOption(spec as VTable.PivotTableConstructorOptions)
-    } else {
-      const vchart = new VChart(spec as ISpec, {
-        dom: div,
-        dpr: 2,
-      })
-      void vchart.renderSync()
-      void vchart.updateSpec(spec as ISpec)
+    const div = document.createElement('div') as unknown as HTMLDivElement
+    div.style.width = '960px'
+    div.style.height = '600px'
+    if (window) {
+      window.URL.createObjectURL = () => 'mocked-url'
+    }
+
+    try {
+      // render
+      if (isPivotChart(builder.vseed)) {
+        const vtable = new VTable.PivotChart(div, spec as VTable.PivotChartConstructorOptions)
+        void vtable.updateOption(spec as VTable.PivotChartConstructorOptions)
+      } else if (isTable(builder.vseed)) {
+        const vtable = new VTable.ListTable(div, spec as VTable.ListTableConstructorOptions)
+        void vtable.updateOption(spec as VTable.ListTableConstructorOptions)
+      } else if (isPivotTable(builder.vseed)) {
+        const vtable = new VTable.PivotTable(div, spec as VTable.PivotTableConstructorOptions)
+        void vtable.updateOption(spec as VTable.PivotTableConstructorOptions)
+      } else {
+        const vchart = new VChart(spec as ISpec, {
+          dom: div,
+          dpr: 2,
+        })
+        void vchart.renderSync()
+        void vchart.updateSpec(spec as ISpec)
+      }
+    } catch (e) {
+      console.error(e)
+      throw e
     }
   } catch (e) {
     console.error(e)
-    throw e
+    expect({expectError: true}).toMatchSnapshot()
+    expect(e).toBeInstanceOf(Error)
   }
 })

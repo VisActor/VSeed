47dbdb905268a085b46793ddaf186060b70fa563
xile611 <dingling112@gmail.com>
2025-10-16 11:33:52 +0800

    fix: remove measureName when only one measure

--- packages/vseed/src/pipeline/advanced/table/pipes/default/defaultMeasureName.ts
+++ packages/vseed/src/pipeline/advanced/table/pipes/default/defaultMeasureName.ts
@@ -1,18 +1,18 @@
 import { MeasureName } from 'src/dataReshape'
 import { intl } from 'src/i18n'
-import type { AdvancedPipe, Dimension } from 'src/types'
+import { findAllMeasures } from 'src/pipeline/utils/measures/find'
+import type { AdvancedPipe, Dimension, DimensionTree } from 'src/types'
 
 export const defaultMeasureName: AdvancedPipe = (advancedVSeed) => {
   const result = { ...advancedVSeed }
-
-  const MeaName: Dimension = {
-    id: MeasureName,
-    alias: intl.i18n`指标名称`,
-  }
+  const measures = findAllMeasures(advancedVSeed.measures as DimensionTree)
 
   // 如果没有指标名称维度，则默认添加指标名称维度
-  if (!result.dimensions?.some((dim) => dim.id === MeasureName)) {
-    result.dimensions?.push(MeaName)
+  if (measures.length > 1 && !result.dimensions?.some((dim) => dim.id === MeasureName)) {
+    result.dimensions?.push({
+      id: MeasureName,
+      alias: intl.i18n`指标名称`,
+    } as Dimension)
   }
 
   return result

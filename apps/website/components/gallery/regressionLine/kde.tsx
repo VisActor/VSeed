61312562c743aedc25fb9e1715c199e274feea96
xile611 <dingling112@gmail.com>
2025-11-05 15:44:23 +0800

    docs: update docs about regressionLine

---
+++ apps/website/components/gallery/regressionLine/kde.tsx
@@ -0,0 +1,14 @@
+import { VSeed } from '@visactor/vseed'
+import { VChartRender } from '../../render/Chart'
+import { memo } from 'react'
+
+export const HistogramKde = memo(() => {
+  const vseed: VSeed = {
+    chartType: 'histogram',
+    dataset: [{ v: 1 }, { v: 1 }, { v: 1 }, { v: 2 }, { v: 5 }, { v: 7 }, { v: 8 }, { v: 9 }, { v: 10 }],
+    kdeRegressionLine: {
+      color: 'red',
+    },
+  }
+  return <VChartRender vseed={vseed} />
+})

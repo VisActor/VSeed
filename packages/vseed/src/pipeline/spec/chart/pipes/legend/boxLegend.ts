4c27a5b428a3bbaaaf0484452208e1658ade4f10
ZJU_czx <952370295@qq.com>
2025-11-17 12:18:24 +0800

    refactor: auto ts formatter

--- packages/vseed/src/pipeline/spec/chart/pipes/legend/boxLegend.ts
+++ packages/vseed/src/pipeline/spec/chart/pipes/legend/boxLegend.ts
@@ -5,15 +5,15 @@ import type { VChartSpecPipe } from 'src/types'
 import { discreteLegend } from './discreteLegend'
 
 export const boxLegend: VChartSpecPipe = (spec, context) => {
-  const normalLegend = discreteLegend(spec, context) as any;
+  const normalLegend = discreteLegend(spec, context) as any
   normalLegend.legends = {
     ...normalLegend.legends,
     data: (data: any[]) => {
-      return data.map(obj => {
-        obj.shape.fill = obj.shape.stroke;
-        return obj;
+      return data.map((obj) => {
+        obj.shape.fill = obj.shape.stroke
+        return obj
       })
-    }
+    },
   }
   return normalLegend
 }

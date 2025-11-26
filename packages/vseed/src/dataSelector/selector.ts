54f6971ab20fb6c87c561a29b7b0ce5ebc6a78bf
xile611 <dingling112@gmail.com>
2025-11-26 17:05:16 +0800

    fix: fix duplicated annotationPoints

--- packages/vseed/src/dataSelector/selector.ts
+++ packages/vseed/src/dataSelector/selector.ts
@@ -41,106 +41,73 @@ export const selector = (
   return selectors[selectorMode === 'And' ? 'every' : 'some']((selector) => {
     // 1. 字符串或数字
     if (isValueSelector(selector)) {
-      if (Object.values(datum).find((v) => v === selector)) {
-        return true
-      }
+      return selectByValue(selector, datum)
     }
 
     // 2. 指标选择器
     else if (isMeasureSelector(selector)) {
-      const op = selector.operator || selector.op
-      const selectorValueArr = Array.isArray(selector.value) ? selector.value : [selector.value]
-
-      switch (op) {
-        case '=':
-          if (
-            String(datum[selector.field]) === String(selectorValueArr[0]) ||
-            nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
-          ) {
-            return true
-          }
-          break
-        case '==':
-          if (datum[selector.field] === selectorValueArr[0]) {
-            return true
-          }
-          break
-        case '!=':
-          if (datum[selector.field] !== selectorValueArr[0]) {
-            return true
-          }
-          break
-        case '>':
-          if (
-            datum[selector.field] > selectorValueArr[0] &&
-            !nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
-          ) {
-            return true
-          }
-          break
-        case '<':
-          if (
-            datum[selector.field] < selectorValueArr[0] &&
-            !nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
-          ) {
-            return true
-          }
-          break
-        case '>=':
-          if (
-            datum[selector.field] >= selectorValueArr[0] ||
-            nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
-          ) {
-            return true
-          }
-          break
-        case '<=':
-          if (
-            datum[selector.field] <= selectorValueArr[0] ||
-            nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
-          ) {
-            return true
-          }
-          break
-        case 'between':
-          if (
-            Array.isArray(selector.value) &&
-            (datum[selector.field] >= selectorValueArr[0] ||
-              nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))) &&
-            (datum[selector.field] <= selectorValueArr[1] ||
-              nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[1])))
-          ) {
-            return true
-          }
-          break
-      }
+      return selectByMeasure(selector, datum)
     }
     // 3. 维度选择器
     else if (isDimensionSelector(selector)) {
-      const op = selector.operator || selector.op
-      const selectorValueArr = Array.isArray(selector.value) ? selector.value : [selector.value]
-      switch (op) {
-        case 'in':
-          if (selectorValueArr.includes(datum[selector.field] as string | number)) {
-            return true
-          }
-          break
-        case 'not in':
-          if (!selectorValueArr.includes(datum[selector.field] as string | number)) {
-            return true
-          }
-          break
-      }
+      return selectByDmension(selector, datum)
     }
     // 4. 部分数据对象选择器
     else if (isPartialDatumSelector(selector)) {
-      if (Object.keys(selector).every((key) => datum[key] === selector[key])) {
-        return true
-      }
+      return selectByPartial(selector, datum)
     }
 
     return false
   })
+}
+
+export const selectorDatum = (datum: Datum, selector: Selector | Selectors | undefined | null): Datum[] => {
+  // 无有效选择器, 则认为全部匹配成功
+  if (!selector) {
+    return []
+  }
+
+  // 统一处理选择器为数组
+  const selectors = (Array.isArray(selector) ? selector : [selector]) as Selectors
+  let finalResult: Datum[] = []
+
+  for (const selector of selectors) {
+    const results: Datum[] = []
+    // 1. 字符串或数字
+    if (isValueSelector(selector)) {
+      Object.entries(datum).forEach(([key, value]) => {
+        if (value === selector) {
+          results.push({ [key]: value } as Datum)
+        }
+      })
+    } else if (isMeasureSelector(selector) && selectByMeasure(selector, datum)) {
+      results.push({ [selector.field]: datum[selector.field] } as Datum)
+    } else if (isDimensionSelector(selector) && selectByDmension(selector, datum)) {
+      results.push({ [selector.field]: datum[selector.field] } as Datum)
+    } else if (isPartialDatumSelector(selector) && selectByPartial(selector, datum)) {
+      results.push(selector)
+    }
+
+    if (results.length) {
+      if (finalResult.length) {
+        finalResult = finalResult.flatMap((prev) => {
+          return results.map((r) => {
+            return {
+              ...prev,
+              ...r,
+            }
+          })
+        })
+      } else {
+        finalResult = results
+      }
+    } else {
+      finalResult = []
+      break
+    }
+  }
+
+  return finalResult
 }
 
 export const isValueSelector = (selector: Selector): selector is ValueSelector => {
@@ -172,4 +139,101 @@ export const isDimensionSelector = (selector: Selector): selector is DimensionSe
     'value' in selector &&
     (['in', 'not in'].includes(selector.operator as string) || ['in', 'not in'].includes(selector.op as string))
   )
+}
+
+export const selectByMeasure = (selector: MeasureSelector, datum: Datum) => {
+  const op = selector.operator || selector.op
+  const selectorValueArr = Array.isArray(selector.value) ? selector.value : [selector.value]
+
+  switch (op) {
+    case '=':
+      if (
+        String(datum[selector.field]) === String(selectorValueArr[0]) ||
+        nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
+      ) {
+        return true
+      }
+      break
+    case '==':
+      if (datum[selector.field] === selectorValueArr[0]) {
+        return true
+      }
+      break
+    case '!=':
+      if (datum[selector.field] !== selectorValueArr[0]) {
+        return true
+      }
+      break
+    case '>':
+      if (
+        datum[selector.field] > selectorValueArr[0] &&
+        !nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
+      ) {
+        return true
+      }
+      break
+    case '<':
+      if (
+        datum[selector.field] < selectorValueArr[0] &&
+        !nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
+      ) {
+        return true
+      }
+      break
+    case '>=':
+      if (
+        datum[selector.field] >= selectorValueArr[0] ||
+        nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
+      ) {
+        return true
+      }
+      break
+    case '<=':
+      if (
+        datum[selector.field] <= selectorValueArr[0] ||
+        nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))
+      ) {
+        return true
+      }
+      break
+    case 'between':
+      if (
+        Array.isArray(selector.value) &&
+        (datum[selector.field] >= selectorValueArr[0] ||
+          nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[0]))) &&
+        (datum[selector.field] <= selectorValueArr[1] ||
+          nearlyEqual(Number(datum[selector.field]), Number(selectorValueArr[1])))
+      ) {
+        return true
+      }
+      break
+  }
+  return false
+}
+
+export const selectByDmension = (selector: DimensionSelector, datum: Datum) => {
+  const op = selector.operator || selector.op
+  const selectorValueArr = Array.isArray(selector.value) ? selector.value : [selector.value]
+  switch (op) {
+    case 'in':
+      if (selectorValueArr.includes(datum[selector.field] as string | number)) {
+        return true
+      }
+      break
+    case 'not in':
+      if (!selectorValueArr.includes(datum[selector.field] as string | number)) {
+        return true
+      }
+      break
+  }
+
+  return false
+}
+
+export const selectByPartial = (selector: PartialDatumSelector, datum: Datum) => {
+  return Object.keys(selector).every((key) => datum[key] === selector[key])
+}
+
+export const selectByValue = (selector: ValueSelector, datum: Datum) => {
+  return Object.values(datum).some((v) => v === selector)
 }
--- packages/vseed/src/pipeline/spec/chart/pipes/annotation/annotationPoint.ts
+++ packages/vseed/src/pipeline/spec/chart/pipes/annotation/annotationPoint.ts
@@ -1,9 +1,12 @@
 import type { ILineChartSpec } from '@visactor/vchart'
-import { selector } from '../../../../../dataSelector'
+import { selectorDatum } from '../../../../../dataSelector'
 import type { Datum, VChartSpecPipe, VSeed } from 'src/types'
 import { isSubset } from './utils'
 import { ANNOTATION_Z_INDEX } from '../../../../utils/constant'
 import { isBarLikeChart } from 'src/pipeline/utils/chatType'
+import { uniqueWith, isDeepEqual } from 'remeda'
+import { findAllMeasures } from 'src/pipeline/utils'
+import { FoldMeasureId } from 'src/dataReshape/constant'
 
 export const annotationPoint: VChartSpecPipe = (spec, context) => {
   const { advancedVSeed, vseed } = context
@@ -26,6 +29,7 @@ export const annotationPoint: VChartSpecPipe = (spec, context) => {
         textAlign: 'center',
         textBaseline: 'top',
       }
+  const allMeasureIds = findAllMeasures(advancedVSeed.reshapeMeasures ?? advancedVSeed.measures).map((m) => m.id)
 
   const markPoint = annotationPointList.flatMap((annotationPoint) => {
     const {
@@ -47,57 +51,83 @@ export const annotationPoint: VChartSpecPipe = (spec, context) => {
     } = annotationPoint
 
     const dataset = advancedVSeed.dataset.flat()
-    const selectedData = selectorPoint ? dataset.filter((datum) => selector(datum, selectorPoint)) : []
+    const selectedData = selectorPoint
+      ? uniqueWith(
+          dataset.flatMap((d) => selectorDatum(d, selectorPoint)),
+          isDeepEqual,
+        )
+      : []
     const dx = -10 - (isHorizontalBar ? (textFontSize as number) : 0) // 由于vchart tag实现问题，需要设置这个强制偏移量
     const dy = isHorizontalBar ? 0 : (textFontSize as number)
-
-    return selectedData.map((datum) => {
-      return {
-        zIndex: ANNOTATION_Z_INDEX,
-        regionRelative: true,
-        coordinate: (data: Datum[]) => {
-          return data.find((item) => isSubset(datum, item))
-        },
-
-        itemLine: {
-          visible: false,
-        },
-        itemContent: {
-          offsetY,
-          offsetX,
-          confine: true,
-          text: {
-            text: text,
+    const baseConfig = {
+      zIndex: ANNOTATION_Z_INDEX,
+      itemLine: {
+        visible: false,
+      },
+      itemContent: {
+        offsetY,
+        offsetX,
+        confine: true,
+        text: {
+          text: text,
+          style: {
+            opacity: 0.95,
+            visible: true,
+            textAlign: textAlign,
+            textBaseline: textBaseline,
+            fill: textColor,
+            stroke: textBackgroundColor,
+            lineWidth: 1,
+            fontSize: textFontSize,
+            fontWeight: textFontWeight,
+            dx,
+            dy,
+          },
+          labelBackground: {
+            visible: textBackgroundVisible,
+            padding: textBackgroundPadding,
             style: {
               opacity: 0.95,
-              visible: true,
-              textAlign: textAlign,
-              textBaseline: textBaseline,
-              fill: textColor,
-              stroke: textBackgroundColor,
-              lineWidth: 1,
-              fontSize: textFontSize,
-              fontWeight: textFontWeight,
+              cornerRadius: textBackgroundBorderRadius ?? 4,
+              fill: textBackgroundColor,
+              stroke: textBackgroundBorderColor,
+              lineWidth: textBackgroundBorderWidth,
               dx,
               dy,
             },
-            labelBackground: {
-              visible: textBackgroundVisible,
-              padding: textBackgroundPadding,
-              style: {
-                opacity: 0.95,
-                cornerRadius: textBackgroundBorderRadius ?? 4,
-                fill: textBackgroundColor,
-                stroke: textBackgroundBorderColor,
-                lineWidth: textBackgroundBorderWidth,
-                dx,
-                dy,
-              },
-            },
           },
         },
-      }
-    })
+      },
+    }
+
+    return vseed.chartType === 'dualAxis'
+      ? spec.series?.reduce((res: any[], s: any, index: number) => {
+          selectedData.forEach((datum: Datum) => {
+            res.push({
+              ...baseConfig,
+              relativeSeriesIndex: index,
+              coordinate: (data: Datum[]) => {
+                const firstItem = data[0]
+                const excludeMeasuresIds = allMeasureIds.filter((id) => id !== firstItem?.[FoldMeasureId])
+
+                return data.find((item) => isSubset(datum, item, excludeMeasuresIds))
+              },
+            })
+          })
+
+          return res
+        }, [] as any[])
+      : selectedData.map((datum: Datum) => {
+          return {
+            ...baseConfig,
+            coordinate: (data: Datum[]) => {
+              const firstItem = data[0]
+              const excludeMeasuresIds = allMeasureIds.filter((id) => id !== firstItem?.[FoldMeasureId])
+
+              return data.find((item) => isSubset(datum, item, excludeMeasuresIds))
+            },
+          }
+        })
   }) as ILineChartSpec['markPoint']
 
   return {

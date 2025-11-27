d423d0f3c03f78ff79497b24355356089cd887c7
xile611 <dingling112@gmail.com>
2025-11-27 12:14:25 +0800

    fix: fix stackCornerRadius of dualAxis chart

--- packages/vseed/src/pipeline/spec/chart/pipes/stack/stackCornerRadius.ts
+++ packages/vseed/src/pipeline/spec/chart/pipes/stack/stackCornerRadius.ts
@@ -1,12 +1,11 @@
 import type { IBarChartSpec } from '@visactor/vchart'
+import { FoldMeasureId } from 'src/dataReshape/constant'
 import type { Datum, VChartSpecPipe, StackCornerRadius } from 'src/types'
 
 export const stackCornerRadius: VChartSpecPipe = (spec, context) => {
   const { advancedVSeed, vseed } = context
   const { chartType } = vseed
   const stackCornerRadius = advancedVSeed.config?.[chartType as 'column']?.stackCornerRadius as StackCornerRadius
-  const { datasetReshapeInfo } = advancedVSeed
-  const { foldInfo } = datasetReshapeInfo[0]
 
   if (chartType === 'dualAxis' && (spec as any).type !== 'bar') {
     return spec
@@ -15,7 +14,7 @@ export const stackCornerRadius: VChartSpecPipe = (spec, context) => {
   return {
     ...spec,
     stackCornerRadius: (_: unknown, datum: Datum) => {
-      if (datum[foldInfo.measureValue] > 0) {
+      if (datum && datum[datum[FoldMeasureId]] > 0) {
         return stackCornerRadius
       }
 

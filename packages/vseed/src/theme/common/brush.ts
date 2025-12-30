0cf7c58481fd19ab87100924f48aaea707b94377
xile611 <dingling112@gmail.com>
2025-12-30 18:00:34 +0800

    fix: fix style of brush

--- packages/vseed/src/theme/common/brush.ts
+++ packages/vseed/src/theme/common/brush.ts
@@ -4,8 +4,6 @@ export const getLightBrushConfig = (): BrushConfig => ({
   enable: false,
   inBrushStyle: {
     opacity: 1,
-    stroke: '#000',
-    lineWidth: 2,
   },
   outOfBrushStyle: {
     opacity: 0.2,
@@ -16,8 +14,6 @@ export const getDarkBrushConfig = (): BrushConfig => ({
   enable: false,
   inBrushStyle: {
     opacity: 1,
-    stroke: '#fff',
-    lineWidth: 2,
   },
   outOfBrushStyle: {
     opacity: 0.25,

f894623df1642e981a1d1b1235396d9923ffb368
jiangtao.yang <jiangtao.yang@bytedance.com>
2025-12-30 15:21:23 +0800

    feat: simplify the vbi builder

--- packages/vbi/src/types/builder/VBIInterface.ts
+++ packages/vbi/src/types/builder/VBIInterface.ts
@@ -1,19 +1,22 @@
 import type { VQueryDSL } from '@visactor/vquery'
 import type { VBIDSL } from '../dsl'
 import type { VSeedDSL } from '@visactor/vseed'
-import { MeasuresBuilder, DimensionsBuilder } from 'src/builder/sub-builders'
+import type { MeasuresBuilder, DimensionsBuilder, ChartTypeBuilder } from 'src/builder/sub-builders'
+import type { Map, Doc, UndoManager } from 'yjs'
 
 export interface VBIBuilderInterface {
+  doc: Doc
+  dsl: Map<any>
+  undoManager: UndoManager
+
+  chartType: ChartTypeBuilder
   measures: MeasuresBuilder
   dimensions: DimensionsBuilder
-  buildVSeed: () => Promise<VSeedDSL>
-  buildVQuery: () => VQueryDSL
-  build: () => VBIDSL
-
-  get vbiDSL(): VBIDSL
 
   applyUpdate: (update: Uint8Array, origin?: any) => void
   encodeStateAsUpdate: (targetStateVector?: Uint8Array) => Uint8Array
-  on: (event: string, listener: (...args: any[]) => void) => void
-  off: (event: string, listener: (...args: any[]) => void) => void
+
+  buildVSeed: () => Promise<VSeedDSL>
+  buildVQuery: () => VQueryDSL
+  build: () => VBIDSL
 }

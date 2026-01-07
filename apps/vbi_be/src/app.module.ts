9aeca54e6449ec41a3e8910f56f3fcea8b28ba55
jiangtao.yang <jiangtao.yang@bytedance.com>
2026-01-07 17:28:44 +0800

    feat: rename the yjs

--- apps/vbi_be/src/app.module.ts
+++ apps/vbi_be/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { YjsGateway } from './yjs/yjs.gateway';
+import { CollaborationGateway } from './collaboration/collaboration.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, YjsGateway],
+  providers: [AppService, CollaborationGateway],
 })
 export class AppModule {}

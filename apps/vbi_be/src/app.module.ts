c0fc18c68ba05ef9a4ba647027899d2387325e2a
jiangtao.yang <jiangtao.yang@bytedance.com>
2026-01-07 17:18:00 +0800

    feat: add yjs gate way

--- apps/vbi_be/src/app.module.ts
+++ apps/vbi_be/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { YjsGateway } from './yjs/yjs.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, YjsGateway],
 })
 export class AppModule {}
--- apps/vbi_be/src/main.ts
+++ apps/vbi_be/src/main.ts
@@ -1,20 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WebSocketServer } from 'ws';
-import { setupWSConnection } from 'y-websocket/bin/utils';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const server = await app.listen(process.env.PORT ?? 3030);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const wss = new WebSocketServer({ server });
-
-  wss.on('connection', (conn, req) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    setupWSConnection(conn, req, { gc: true });
-  });
+  app.useWebSocketAdapter(new WsAdapter(app));
+  await app.listen(process.env.PORT ?? 3030);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 };

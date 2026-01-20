f3ddb801bcf5476a8eef71418c9252f7ada0c788
jiangtao.young <jiangtao.young@qq.com>
2026-01-20 17:13:57 +0800

    fix: demo dataset url error

--- packages/vbi/tests/query/select.test.ts
+++ packages/vbi/tests/query/select.test.ts
@@ -37,7 +37,7 @@ describe('select', () => {
         },
         query: async ({ queryDSL, schema }) => {
           if (!(await vquery.hasDataset(connectorId))) {
-            const url = 'https://visactor.github.io/VSeed/dataset/supermarket.csv'
+            const url = 'https://visactor.github.io/VBI/dataset/supermarket.csv'
             const datasetSource = { type: 'csv', rawDataset: url }
             await vquery.createDataset(connectorId, schema as DatasetColumn[], datasetSource as RawDatasetSource)
           }
